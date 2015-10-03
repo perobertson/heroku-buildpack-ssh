24c36daea46a4e0a5209513d87da1e9ef48be1d4
Paul Robertson <t.paulrobertson@gmail.com>
2015-10-03 14:49:11 -0400

    remove test files

--- build/.profile.d/init_known_hosts.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-ssh-keyscan github.com >> "${HOME}/.ssh/known_hosts"
-ssh-keyscan bitbucket.com >> "${HOME}/.ssh/known_hosts"
-ssh-keyscan localhost >> "${HOME}/.ssh/known_hosts"
